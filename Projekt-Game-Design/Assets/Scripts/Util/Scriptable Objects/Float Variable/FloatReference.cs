0af84e4e1ee51325121088c77199b09bdc877a03
Vincent Jung <vincent.a.jung@gmail.com>
2021-09-01 11:26:23 +0200

    added Float Variable to Util

---
+++ Projekt-Game-Design/Assets/Scripts/Util/Scriptable Objects/Float Variable/FloatReference.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Util.ScriptableObjects {
+    
+    [Serializable]
+    public class FloatReference {
+        public bool UseConstant = true;
+        public float ConstantValue;
+        public FloatVariable Variable;
+        
+        public float Value {
+            get { return UseConstant ? ConstantValue : Variable.Value; }
+        }
+    }
+}
