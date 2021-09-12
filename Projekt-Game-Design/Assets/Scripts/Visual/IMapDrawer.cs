e1573af023a289853cd562288565f8cdaa230292
Vincent Jung <vincent.a.jung@gmail.com>
2021-09-13 00:27:41 +0200

    added IMapDrawer as an Interface for alle Map Drawers

---
+++ Projekt-Game-Design/Assets/Scripts/Visual/IMapDrawer.cs
@@ -0,0 +1,10 @@
+
+
+using UnityEngine;
+
+public interface IMapDrawer
+{
+    public void DrawGrid();
+    public void DrawBoxCursorAt(Vector3 start, Vector3 end);
+    public void DrawCursorAt(Vector3 pos);
+}
