4de563ffcc6c5d254b41a545d78dc21de8b88a95
Vincent Jung <vincent.a.jung@gmail.com>
2021-09-15 10:23:13 +0200

    added GraphContainer, NodeGraph, GraphGenerator

---
+++ Projekt-Game-Design/Assets/Scripts/Graph/ScriptableObjects/GraphContainerSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Graph.ScriptableObjects {
+    [CreateAssetMenu(fileName = "newGraphContainer", menuName = "Grid/GraphContainer", order = 0)]
+    public class GraphContainerSO : ScriptableObject {
+        public List<NodeGraph> basicMovementGraph;
+    }
+}
