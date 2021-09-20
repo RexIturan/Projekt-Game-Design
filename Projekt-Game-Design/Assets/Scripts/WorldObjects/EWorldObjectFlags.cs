1ed9d6b8b32493d3f9ecc431bf2e58a6aaadc721
Janus-Jeromin <84479155+Janus-Jeromin@users.noreply.github.com>
2021-09-20 11:50:11 +0200

    world objects
    scriptable object, script,
    example scriptable object and prefab

---
+++ Projekt-Game-Design/Assets/Scripts/WorldObjects/EWorldObjectFlags.cs
@@ -0,0 +1,8 @@
+[System.Serializable, System.Flags]
+public enum EWorldObjectFlags
+{
+    ATTACKABLE = 1,
+    PASSABLE = 2,
+    VISIBLE = 4,
+    PIERCEABLE = 8
+}
---
+++ Projekt-Game-Design/Assets/Scripts/WorldObjects/ScriptableObjects/WorldObjectTypeSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// type of world object 
+//
+[CreateAssetMenu(fileName = "New WorldObject", menuName = "WorldObject")]
+public class WorldObjectTypeSO : ScriptableObject
+{
+    [SerializeField] public GameObject prefab;
+    [SerializeField] public EWorldObjectFlags initialProperties; // visiable, destructable, etc. 
+    [SerializeField] public int maxLifePoints;
+
+    [SerializeField] public object[] actions; // not player actions;
+                                              // events with conditions and effect
+}
