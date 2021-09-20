a154b821930a16d6bc104145b30f6e5563fc27bd
Vincent Jung <vincent.a.jung@gmail.com>
2021-09-20 12:00:28 +0200

    added saveslots, added int event channels, seperated save/load - game/map

---
+++ Projekt-Game-Design/Assets/Scripts/Events/ScriptableObjects/IntEventChannelSO.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Events.ScriptableObjects {
+    [CreateAssetMenu(menuName = "Events/Int Event Channel")]
+    public class IntEventChannelSO : EventChannelBaseSO {
+        
+        public UnityAction<int> OnEventRaised;
+
+        public void RaiseEvent(int value)
+        {
+            if (OnEventRaised != null)
+                OnEventRaised.Invoke(value);
+        }
+    }
+}
