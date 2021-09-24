2ed308b592411af4e96bc0ca51e239ddad922f0f
Janus-Jeromin <84479155+Janus-Jeromin@users.noreply.github.com>
2021-09-24 12:44:54 +0200

    StateMachine Player
    Man kann jetzt nur noch einen Charakter auswaehlen
    Die Charaktere faerben sich jetzt eigenstaendig ueber eine eigene Aktion
    Die TurnStateMachine blendet jetzt das Aktionsmenue ein oder aus, je nach dem
    Einige Aktionen werden nicht mehr genutzt. Ich wollte sie aber noch nicht loeschen, falls sie doch noch irgendwie wichtig waren.

---
+++ Projekt-Game-Design/Assets/Scripts/TurnController/Actions/SetAbilitiesVisibitySO.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Events.ScriptableObjects;
+using UOP1.StateMachine;
+using UOP1.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "SetAbilitiesVisibity", menuName = "State Machines/Actions/TurnState/SetAbilitiesVisibity")]
+public class SetAbilitiesVisibitySO : StateActionSO
+{
+    [Header("Sending Events On")]
+    [SerializeField] private BoolEventChannelSO setActionMenuVisibility;
+    [SerializeField] private bool visible;
+
+    protected override StateAction CreateAction() => new SetAbilitiesVisibity(setActionMenuVisibility, visible);
+}
+
+public class SetAbilitiesVisibity : StateAction
+{
+    private BoolEventChannelSO setActionMenuVisibility;
+    private bool show;
+    // private TurnContainerCO turnController;
+
+    public SetAbilitiesVisibity(BoolEventChannelSO ActionMenuVisibilityEvent, bool visibility)
+    {
+        setActionMenuVisibility = ActionMenuVisibilityEvent;
+        show = visibility;
+    }
+
+    public override void Awake(StateMachine stateMachine)
+	{
+		// turnController = stateMachine.GetComponent<TurnContainerCO>();
+	}
+	
+	public override void OnUpdate()
+	{
+	}
+	
+	public override void OnStateEnter()
+    {
+        setActionMenuVisibility.RaiseEvent(show);
+	}
+	
+	public override void OnStateExit()
+	{
+
+	}
+}
