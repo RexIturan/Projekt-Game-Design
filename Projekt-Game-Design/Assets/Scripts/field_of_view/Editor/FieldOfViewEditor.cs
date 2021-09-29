d4b50d5975dbd6e0d626ae30f14abbe415b605da
Jan <jan.pilar@gmx.de>
2021-09-29 20:05:08 +0200

    fov algorith first version implemented

---
+++ Projekt-Game-Design/Assets/Scripts/field_of_view/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,19 @@
+﻿using Graph;
+using UnityEditor;
+using UnityEngine;
+
+namespace field_of_view.Editor {
+    [CustomEditor(typeof(FieldOfView))]
+    public class FieldOfViewEditor : UnityEditor.Editor {
+        public override void OnInspectorGUI() {
+            DrawDefaultInspector();
+
+            var fieldOfViewGenerator = (FieldOfView) target;
+
+            if (GUILayout.Button("generateVisionString")) {
+                fieldOfViewGenerator.generateVision();
+            }
+            
+        }
+    }
+}
