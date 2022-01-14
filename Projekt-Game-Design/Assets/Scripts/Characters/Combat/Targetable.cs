7be44a9ca29e461ac48fb3138dc99ad981e83d4a
Vincent Jung <vincent.a.jung@gmail.com>
2022-01-14 23:11:01 +0100

    added healthbar

--- Projekt-Game-Design/Assets/Scripts/Characters/Combat/Targetable.cs
+++ Projekt-Game-Design/Assets/Scripts/Characters/Combat/Targetable.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using Characters;
 using UnityEngine;
+using Visual.Healthbar;
 
 namespace Combat {
 	//todo Targatsble Or Damageable
@@ -28,6 +29,12 @@ namespace Combat {
 		
 		public void ReceivesDamage(int damage) {
 			statistics.StatusValues.HitPoints.Decrease(damage);
+			
+			if ( IsDead ) {
+				var healthbar = GetComponentInChildren<HealthbarController>();
+				healthbar.UpdateVisuals();
+				healthbar.StartHideAfterDelay();	
+			}
 		}
 
 		public Vector3Int GetGridPosition() {
