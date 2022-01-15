adc29bc7a16f268f59f0d077eadc0e2e39fef7c9
Vincent Jung <vincent.a.jung@gmail.com>
2022-01-15 01:27:32 +0100

    added sliding door

---
+++ Projekt-Game-Design/Assets/Scripts/WorldObjects/New/Doors/DoorAnimator.cs
@@ -0,0 +1,22 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace WorldObjects.Doors {
+	public class DoorAnimator : MonoBehaviour {
+		[SerializeField] private Transform doorModel;
+		
+		[SerializeField] private Vector3 startPos;
+		[SerializeField] private Vector3 moveDistance;
+		[SerializeField] private float moveCycleLength;
+		[SerializeField] private Ease moveEase = Ease.Linear;
+		
+		public void OpenDoor() {
+			var targetPos = startPos + moveDistance;
+			doorModel.DOLocalMove(targetPos, moveCycleLength)
+				.SetEase(moveEase).SetLoops(2, LoopType.Yoyo)
+				.OnComplete(() => {
+					//do stuff -> maybe callback um das pathfinding zu updaten?
+				});
+		}
+	}
+}
---
+++ Projekt-Game-Design/Assets/Scripts/WorldObjects/New/Doors/LockAnimator.cs
@@ -0,0 +1,25 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace WorldObjects.Doors {
+	public class LockAnimator : MonoBehaviour {
+		[SerializeField] private Transform lockModel;
+		[SerializeField] private MeshRenderer meshRenderer;
+		
+		[SerializeField] private Material activeMaterial;
+		[SerializeField] private Material inctiveMaterial;
+		
+		[SerializeField] private Vector3 startPos;
+		[SerializeField] private Vector3 moveDistance;
+		[SerializeField] private float moveCycleLength;
+		[SerializeField] private bool openRight;
+
+		public void OpenLock() {
+			var targetPos = startPos;
+			targetPos += openRight ? moveDistance : -moveDistance;
+			lockModel.DOLocalMove(targetPos, moveCycleLength)
+				.SetEase(Ease.Linear)
+				.OnComplete(() => { meshRenderer.material = activeMaterial;});
+		}
+	}
+}
