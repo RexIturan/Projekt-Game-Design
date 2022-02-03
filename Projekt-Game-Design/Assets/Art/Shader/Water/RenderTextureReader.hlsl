128b5d96f66b0a24c1071797f09ad1091725f27c
Vincent Jung <vincent.a.jung@gmail.com>
2022-02-03 04:38:54 +0100

    shader and vfx stuff + UI Toolkit Shape Drawing Stuff

---
+++ Projekt-Game-Design/Assets/Art/Shader/Water/RenderTextureReader.hlsl
@@ -0,0 +1,16 @@
+﻿//UNITY_SHADER_NO_UPGRADE
+#ifndef MYHLSLINCLUDE_INCLUDED
+#define MYHLSLINCLUDE_INCLUDED
+
+SamplerState MeshTextureSampler
+{
+    Filter = MIN_MAG_MIP_LINEAR;
+    AddressU = Wrap;
+    AddressV = Wrap;
+};
+
+void DepthFromRenderTexture_float(Texture2D _texture, float2 uv, out float3 Out) {
+    float3 color = _texture.Sample(MeshTextureSampler, uv);
+    Out = color;
+}
+#endif //MYHLSLINCLUDE_INCLUDED
--- Projekt-Game-Design/Assets/Scripts/Util/MeshUtils/MeshCreator.cs
+++ Projekt-Game-Design/Assets/Scripts/Util/MeshUtils/MeshCreator.cs
@@ -9,6 +9,7 @@ namespace GDP01.Util.MeshUtils {
 			// string filePath = 
 			// 	EditorUtility.SaveFilePanelInProject("Save Procedural Mesh", $"{name}", "asset", "");
 			string filePath = $"Assets/Art/Models/level/{name}.asset";
+			//todo use this -> EditorUtility.CopySerialized
 			AssetDatabase.CreateAsset(mesh, filePath);
 #endif
 			return mesh;
