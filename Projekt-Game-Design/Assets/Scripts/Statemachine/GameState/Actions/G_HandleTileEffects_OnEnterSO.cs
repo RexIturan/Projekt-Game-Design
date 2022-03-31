4de8fe16af64f0ceec1c34a8fa4cbea60dfda078
Janus-Jeromin <84479155+Janus-Jeromin@users.noreply.github.com>
2022-03-31 09:19:30 +0200

    Tile Effects are now updated when player turn is next

--- Projekt-Game-Design/Assets/Scripts/Statemachine/GameState/Actions/G_HandleTileEffects_OnEnterSO.cs
+++ Projekt-Game-Design/Assets/Scripts/Statemachine/GameState/Actions/G_HandleTileEffects_OnEnterSO.cs
@@ -1,3 +1,4 @@
+using Characters.Types;
 using GameManager;
 using UnityEngine;
 using UOP1.StateMachine;
@@ -6,23 +7,36 @@ using UOP1.StateMachine.ScriptableObjects;
 [CreateAssetMenu(fileName = "g_HandleTileEffects_OnEnter", menuName = "State Machines/Actions/GameState/Handle Tile Effects On Enter")]
 public class G_HandleTileEffects_OnEnterSO : StateActionSO
 {
+		[SerializeField] private Faction onFactionsTurn;
 		[SerializeField] private VoidEventChannelSO handleTileEffectsEC;
 
-		public override StateAction CreateAction() => new G_HandleTileEffects_OnEnter(handleTileEffectsEC);
+		public override StateAction CreateAction() => new G_HandleTileEffects_OnEnter(handleTileEffectsEC, onFactionsTurn);
 }
 
 public class G_HandleTileEffects_OnEnter : StateAction
 {
 		private VoidEventChannelSO handleTileEffectsEC;
+		private Faction onFactionsTurn;
 
-		public G_HandleTileEffects_OnEnter(VoidEventChannelSO handleTileEffectsEC) {
+		private GameSC _gameSC;
+
+		public override void Awake(StateMachine stateMachine)
+		{
+				_gameSC = stateMachine.GetComponent<GameSC>();
+		}
+
+		public G_HandleTileEffects_OnEnter(VoidEventChannelSO handleTileEffectsEC, Faction onFactionsTurn)
+		{
 				this.handleTileEffectsEC = handleTileEffectsEC;
+				this.onFactionsTurn = onFactionsTurn;
 		}
 
 		public override void OnUpdate() { }
 
-		public override void OnStateEnter() {
-				handleTileEffectsEC.RaiseEvent();
+		public override void OnStateEnter()
+		{
+				if ( _gameSC.CurrentPlayer.Equals(onFactionsTurn) )
+						handleTileEffectsEC.RaiseEvent();
 		}
 
 		public override void OnStateExit() { }
